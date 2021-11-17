87b0ce30502fd1762a30bd22b5165e4566bbcf16
Aniket <aniketkumarpaul1@gmail.com>
2021-11-17 22:48:03 +0530

    Basic JS

---
+++ JSTut.js
@@ -0,0 +1,13 @@
+// JavaScript
+// ->client side scripting language
+// ->can act as both client as well as server side language
+
+// What can we do IN-BROWSER JS?
+// ->Add new HTML and change existing HTML from DOM(Document Object Model)(html, style, body, navbar, div etc.)
+// ->React to Events(response from server, key press, mouse movement etc .)
+// ->AJAX(Asynchronous JS And XML) Requests(GET/POST requests) - helps in changing contents of webpage dynamically without Reloading
+// ->Get & Set Cookies & Use localStorage
+
+// What can't we do IN-BROWSER JS?
+// ->read/write to & from computer hard disk without Permission
+// ->can't access cross site data/same OriginPolicy by default
