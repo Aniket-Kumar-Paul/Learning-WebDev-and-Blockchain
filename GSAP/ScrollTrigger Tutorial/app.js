9e4ceac4738529cec438180c8aa3590f6087b074
Aniket <aniketkumarpaul1@gmail.com>
2022-08-06 10:56:29 +0530

    start/end properties

--- GSAP/ScrollTrigger Tutorial/app.js
+++ GSAP/ScrollTrigger Tutorial/app.js
@@ -7,8 +7,30 @@
 // to play animation only when we scroll to the element, we use scrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square1", { // animate .square1
+gsap.to(".square", { // animate .square
     x: 700,
     duration: 3,
-    scrollTrigger: '.square2' // play only when .square2 element comes in viewport
+    scrollTrigger: {
+        trigger: '.square', 
+
+        // start/end: px,%,string,function etc..
+        // start: 400, // start animation after 400px scrolling
+        // start: "top center", // when top of trigger element meets center of the viewport, start the animation (top, center, bottom)
+        start: "top 30%", // when top of trigger element meets 30% of the viewport, start the animation
+        
+        // end: "center 20%",
+        end: () => `+=${document.querySelector(".square").offsetHeight}`, // end = height of square in px
+        // .offsetHeight -> return height of element including padding and border as an interger px
+
+        markers: true, // shows scroll start, end markers etc for development purpose
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "fuchsia",
+        //     fontSize: "4rem",
+        //     indent: 200
+        // }
+        
+        // when animation starts, it will toggle to this class(red), and toggles back to default when animation ends 
+        toggleClass: "red" // [make red class in css file]
+    }
 })
