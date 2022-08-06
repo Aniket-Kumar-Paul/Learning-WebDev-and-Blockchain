7b259d747900d9fdfc8e89f0ebccb6d2a972c8eb
Aniket <aniketkumarpaul1@gmail.com>
2022-08-06 11:15:51 +0530

    scrub - animates only when scrolling

--- GSAP/ScrollTrigger Tutorial/app.js
+++ GSAP/ScrollTrigger Tutorial/app.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
     x: 700,
-    duration: 3,
+    duration: 90,
     scrollTrigger: {
         trigger: '.square', 
         start: "top 60%",
@@ -12,6 +12,11 @@ gsap.to(".square", {
         // toggleActions: "play     none     none         none", // default actions for below events
         // Events -->      onEnter  onLeave  onEnterBack  onLeaveBack
         // Actions --> play, reverse, pause, resume, reset(initial position), complete(final animation positon), none etc..
-        toggleActions: "restart reverse none none"
+        toggleActions: "restart reverse none none",
+
+        // animates only when scrolling(in the scroller range ofcourse), animation stops if scrolling stops
+        // ignores duration attribute
+        // scrub: true/false, duration in sec for smoothness
+        scrub: 5
     }
 })
