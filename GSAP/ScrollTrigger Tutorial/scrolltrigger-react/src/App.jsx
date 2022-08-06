a4a7d6b0d10f69bf7f7062dfae8745e53e77dc12
Aniket <aniketkumarpaul1@gmail.com>
2022-08-06 12:29:38 +0530

    using gsap(scrolltrigger) with react

---
+++ GSAP/ScrollTrigger Tutorial/scrolltrigger-react/src/App.jsx
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+function App() {
+  const imgRef = useRef(null);
+  useEffect(()=>{
+    const el = imgRef.current;
+    
+    gsap.fromTo(el, {
+      rotation: 0
+    }, {
+      rotation: 180,
+      duration: 3,
+      scrollTrigger: {
+        trigger: el
+      }
+    })
+  },[])
+
+  return (
+    <div className="App">
+      <div className="helper"></div>
+      <img ref={imgRef} src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
