7f9d7efd132324b972143f479be2a0e140b112bf
Aniket <aniketkumarpaul1@gmail.com>
2022-08-18 23:21:06 +0530

    added withdraw function, various withdraw methods-transfer, send, call

--- Blockchain/contracts/FundMe.sol
+++ Blockchain/contracts/FundMe.sol
@@ -1,40 +1,53 @@
-// Get funds from users
-// Owner of the contract(the one who deploys) can withdraw funds
-// set a minimum funding value in USD
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "./PriceConverter.sol"
+import "./PriceConverter.sol";
 
 contract FundMe {
-    using PriceConverter for uint256; // we can use functions in library using . for uint256 variables
+    using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 50 * 1e18; // 50 dollar (but to match with converted value, 1e18)
+    uint256 public minimumUSD = 50 * 1e18; 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    // public - anyone can call the function
-    // payable - the function can send and receive ether into the contract
-    // smart contracts can hold funds just like how wallets can
-    function fund() public payable{ 
-        // msg - global variable that has properties which allow access to blockchain
-        // msg.sender -> address where current(external) function call came from
-        // msg.data, msg.gas, msg.sig, msg.value(amount of wei sent)
-        
-        // set min. value as 1eth
-        // if its less than revert back with the message
-        // reverting -> undo any action before, & send remaining gas back
-        // require(msg.value > 1e18, "Didn't send enough!"); // 1e18 = 1 * 10^18 = 10^18 wei = 1eth
+    function fund() public payable{
         require(msg.value.getConversionRate() > minimumUSD, "Didn't send enough!");
-        // msg.value gets passed as the first argument to the getConversionRate function
-        // msg.value has 18 decimal places (1ETH = 10^18wei)
 
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = msg.value; 
+        addressToAmountFunded[msg.sender] += msg.value; 
     }
 
-    // function withdraw() {
+    function withdraw() public{
+        // setting funding amount of funders to 0
+        for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-    // }
+        // reset funders array
+        funders = new address[](0); // 0 is size
+
+        // withdraw the funds
+        // We can send ether to other contracts by 
+            // - TRANSFER (2300gas, throws error if more gas is used)
+            // - SEND (2300gas, returns bool)
+            // - CALL (forward all gas or set gas, returs bool)
+
+
+        // TRANSFER - transfer funds to address who is calling withdraw function
+        // msg.sender-the address that has originated the call
+        // convert msg.sender from address type to payable address
+        // this -> refers to the whle contract
+        // payable(msg.sender).transfer(address(this).balance); 
+        // transfer automatically reverts transaction if it fails
+
+        // SEND
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance); 
+        //require(sendSuccess, "Send Failed!"); // require is necessary for 'send' to revert the transaction if it fails
+
+        // CALL (recommended to send & receive ethereum)
+        // .call(info about any function call)
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}(""); // dataReturned will have any values from functions inside .call(..)
+        require(callSuccess, "Call Failed");
+    }
 }
--- Blockchain/contracts/PriceConverter.sol
+++ Blockchain/contracts/PriceConverter.sol
@@ -3,19 +3,13 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice() internal view returns(uint256){ // returns value of 1ETH in USD
-        // using chainlink data feeds
-        // Address (Go to chainlink ethereum data feeds, search for ETH/USD for your testnet) - 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        // ABI
+    function getPrice() internal view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        // (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
-        (,int256 price,,,) = priceFeed.latestRoundData(); // we only need price
-        // the above is price of ETH in USD
-        // there are 8 decimal places associated with the price field
-        return uint256(price * 1e10); // to match with decimal places of msg.value
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
     }   
 
-    function getConversionRate(uint256 ethAmount) internal view returns (uint256){ // Convert ETH to USD
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUSD;
