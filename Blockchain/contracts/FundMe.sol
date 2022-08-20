5b8e968c27228ea2cd2cb417ce46f57662c1e8d7
Aniket <aniketkumarpaul1@gmail.com>
2022-08-20 08:48:53 +0530

    Making the contract gas efficient - using constant, immutable

--- Blockchain/contracts/FundMe.sol
+++ Blockchain/contracts/FundMe.sol
@@ -3,21 +3,31 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+// For variables, which get set only once(Eg.minimumUSD, owner), 
+// we can use constant, immutable keywords to make it more gas efficient
+// they save gas because these variables are directly stored into the byte code
+// of the contract instead of storage
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 50 * 1e18; 
+    // constants must be declared and initialized in same line
+    // constants must be all caps with _
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    // immutable is similar to constant but can be initialized later
+    // naming convention is using i_ as a prefix
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender; // whoever deployed the contract
+        i_owner = msg.sender; // whoever deployed the contract
     }
 
     function fund() public payable{
-        require(msg.value.getConversionRate() > minimumUSD, "Didn't send enough!");
+        require(msg.value.getConversionRate() > MINIMUM_USD, "Didn't send enough!");
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value; 
@@ -39,11 +49,7 @@ contract FundMe {
     }
 
     modifier onlyOwner {
-        // _ means the code inside function which which will have this modifier
-        // any function with this modifier will first call the below require statement
-        // and then run the function code
-        // but if _ was above, then first function code is run and then the requrie statement
-        require(msg.sender == owner, "Only Owner can withdraw funds!");
+        require(msg.sender == i_owner, "Only Owner can withdraw funds!");
         _;
     }
 }
