8788b6762be555268d80a3eb2b4f43a7a20b4457
Aniket <aniketkumarpaul1@gmail.com>
2022-10-04 11:59:12 +0530

    added connect button

--- Blockchain/smart-contract-lottery [Full stack]/client/pages/_app.js
+++ Blockchain/smart-contract-lottery [Full stack]/client/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        // initializeOnMount is optionality to hook into a server to add more features to the website
+        // but in this project we are not using any server
+        <MoralisProvider initializeOnMount={false}>
+            <Component {...pageProps} />
+        </MoralisProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
