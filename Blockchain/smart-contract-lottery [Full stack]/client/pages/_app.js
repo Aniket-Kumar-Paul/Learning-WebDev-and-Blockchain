b13215a51dbf108460b315d9cd4bb729e6264d9d
Aniket <aniketkumarpaul1@gmail.com>
2022-10-08 21:28:53 +0530

    using runContractFunction to run functions of the contract, notifications on success and more..

--- Blockchain/smart-contract-lottery [Full stack]/client/pages/_app.js
+++ Blockchain/smart-contract-lottery [Full stack]/client/pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "@web3uikit/core";
 
 function MyApp({ Component, pageProps }) {
     return (
         // initializeOnMount is optionality to hook into a server to add more features to the website
         // but in this project we are not using any server
         <MoralisProvider initializeOnMount={false}>
-            <Component {...pageProps} />
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
         </MoralisProvider>
     );
 }
