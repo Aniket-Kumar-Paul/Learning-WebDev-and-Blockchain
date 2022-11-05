51fdd3b5aac55aaa2859c8be7fd5d262dbb222ae
Aniket <aniketkumarpaul1@gmail.com>
2022-11-05 11:42:48 +0530

    Hardhat DeFi (Programmatic Borrowing and Lending) - depositing ETH and getting WETH

---
+++ Blockchain/hardhat-defi/README.md
@@ -0,0 +1,4 @@
+What do we want to do (programatically) ?
+1. Deposit collateral: ETH / WETH (Wrapped ETH)
+2. Borrow another asset: (DAI is a stablecoin - always equal to 1 dollar)
+3. Repay the DAI 
---
+++ Blockchain/hardhat-defi/scripts/getWeth.js
@@ -0,0 +1,26 @@
+// script to deposit our token(ETH) and get back WETH token
+// withdrawing ETH back from WETH is called burning WETH
+
+const { getNamedAccounts, ethers } = require("hardhat")
+const AMOUNT = ethers.utils.parseEther("0.02")
+
+async function getWeth() {
+    const { deployer } = await getNamedAccounts()
+
+    // we need to call "deposit" function on weth contract
+    // so we create IWeth interface
+    // compile the interface to get the abi
+    // get WETH mainnet contract address https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+    // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        deployer
+    )
+    const tx = await iWeth.deposit({ value: AMOUNT })
+    await tx.wait(1)
+    const wethBalance = await iWeth.balanceOf(deployer)
+    console.log(`Got ${wethBalance.toString()}WETH`)
+}
+
+module.exports = { getWeth }
