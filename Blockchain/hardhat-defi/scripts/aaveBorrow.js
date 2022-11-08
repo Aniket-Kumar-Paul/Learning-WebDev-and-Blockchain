b300798577d4a0be1722230b3264c1e99d4d8fd9
Aniket <aniketkumarpaul1@gmail.com>
2022-11-08 21:28:23 +0530

    borrowing from aave

--- Blockchain/hardhat-defi/scripts/aaveBorrow.js
+++ Blockchain/hardhat-defi/scripts/aaveBorrow.js
@@ -6,6 +6,8 @@ async function main() {
     await getWeth()
     const { deployer } = await getNamedAccounts()
 
+    // DEPOSIT
+
     // to interact with the aave protocol, we again need the abi and address
     // get the lending pool address using lending pool address provider and abi
     // https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
@@ -21,6 +23,53 @@ async function main() {
     // deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode)
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited!")
+
+    // BORROW
+
+    // how much we have borrowed, how much we have in collateral, how much we can borrow ?
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+
+    // what is the conversion rate of DAI ?
+    // we want to know how much we can borrow (availableBorrowsETH) in DAI.
+    const daiPrice = await getDaiPrice()
+    const amountDaiToBorrow =
+        (await availableBorrowsETH.toString()) * 0.95 * (1 / daiPrice.toNumber())
+    console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+    const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString())
+
+    // borrow time!
+    // https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f
+    const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+    await getBorrowUserData(lendingPool, deployer)
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrowWei, account) {
+    // .borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
+    const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrowWei, 1, 0, account)
+    await borrowTx.wait(1)
+    console.log("You've borrowed successfully!")
+}
+
+async function getDaiPrice() {
+    // https://docs.chain.link/docs/data-feeds/price-feeds/addresses/
+    const daiEthPriceFeed = await ethers.getContractAt(
+        "AggregatorV3Interface",
+        "0x773616E4d11A78F511299002da57A0a94577F1f4" // DAI/ETH price feed address
+    )
+    const price = (await daiEthPriceFeed.latestRoundData())[1] // [1] is the answer
+    console.log(`The DAI/ETH price is ${price.toString()}`)
+    return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+    // https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool
+    const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+        await lendingPool.getUserAccountData(account)
+    console.log(`You have ${totalCollateralETH} worth of ETH deposited.`)
+    console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
+    console.log(`You can borrow ${availableBorrowsETH} worth of ETH`)
+    return { availableBorrowsETH, totalDebtETH }
 }
 
 async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
