34d750a11f27722453dd8e1465df1e9dbfbf57d6
Aniket <aniketkumarpaul1@gmail.com>
2022-11-23 23:50:43 +0530

    RANDOM IPFS NFT - Uploading token images (using Pinata here) and getting the tokenURIs

--- Blockchain/hardhat-nft/deploy/02-deploy-random-ipfs.js
+++ Blockchain/hardhat-nft/deploy/02-deploy-random-ipfs.js
@@ -1,11 +1,32 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
 const { verify } = require("../utils/verify")
+
+const imagesLocation = "./images/randomNft/"
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+}
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+
+    let tokenUris
+    // get the IPFS hashes of images
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
 
     let vrfCoordinatorV2Address, subscriptionId
 
@@ -21,13 +42,42 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     }
     log("--------------------------------------------------------------------")
 
-    const args = [
-        // constructor arguments for random-ipfs
-        vrfCoordinatorV2Address,
-        subscriptionId,
-        networkConfig[chainId].gasLane,
-        networkConfig[chainId].callbackGasLimit,
-        // tokenURIs
-        networkConfig[chainId].mintFee,
-    ]
+    await storeImages(imagesLocation)
+    // const args = [
+    //     // constructor arguments for random-ipfs
+    //     vrfCoordinatorV2Address,
+    //     subscriptionId,
+    //     networkConfig[chainId].gasLane,
+    //     networkConfig[chainId].callbackGasLimit,
+    //     // tokenURIs
+    //     networkConfig[chainId].mintFee,
+    // ]
 }
+
+async function handleTokenUris() {
+    // will return an array of token URIs
+    tokenUris = []
+
+    // store the images in ipfs and get the response(hashes) of the images
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+
+    // for each response, create the meta data and upload it
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} puppy!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+
+        // store the JSON to pinata/IPFS
+        const metadataUplaodResponse = await storeTokenUriMetadata(tokenUriMetadata)
+
+        tokenUris.push(`ipfs://${metadataUplaodResponse.IpfsHash}`)
+    }
+
+    console.log("Token URIs uploaded. The are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
