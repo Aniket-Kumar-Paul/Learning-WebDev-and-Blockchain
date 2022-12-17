5bb713b541b35b9ca59ef0896bbff65fe46e97ab
Aniket <aniketkumarpaul1@gmail.com>
2022-12-17 12:29:36 +0530

    Added moralis local server

--- Blockchain/hardhat-nft-marketplace [Full Stack]/moralis-frontend/pages/index.js
+++ Blockchain/hardhat-nft-marketplace [Full Stack]/moralis-frontend/pages/index.js
@@ -1,14 +1,23 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+    // How do we show the recently listed NFTs?
+
+    // We will index the emitted events off-chain & then read from our database
+    // |_ Setup a server to listen for those events to be fired, & we will add them to a database to query 
+
+    // We can do this using Moralis server (centralized & many features) or The Graph (decentralized)
+    // [The Graph -> an indexing protocol for querying networks like Ethereum & IPFS]
+
+    // Centralized Server :-
+    // All logic is still 100% on chain 
+    // Speed & Development time 
+    // hard to start a production blockchain project 100% decentralized 
+    // can create more features with a centralized back end to start 
+
     return (
         <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+            
             
             Homepage
         </div>
