a7ee293577a59770f05962f68209cfebf5033e6f
Aniket <aniketkumarpaul1@gmail.com>
2023-01-16 13:03:34 +0530

    SIMPLE DAO - governor contract deploy script

--- Blockchain/simple-dao/helper-hardhat-config.ts
+++ Blockchain/simple-dao/helper-hardhat-config.ts
@@ -1,1 +1,4 @@
 export const MIN_DELAY = 3600; // 1 hour
+export const VOTING_PERIOD = 5; // 5 blocks
+export const VOTING_DELAY = 1; //1 block
+export const QUORUM_PERCENTAGE = 4; // 4% of voters need to have voted for a vote to pass
