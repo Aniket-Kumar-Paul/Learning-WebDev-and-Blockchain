9aa473e4cc5f6a0c737e6394b56187410eaa6a06
Aniket <aniketkumarpaul1@gmail.com>
2023-01-16 12:55:31 +0530

    SIMPLE DAO - time lock deploy script

--- Blockchain/simple-dao/deploy/01-deploy-governor-token.ts
+++ Blockchain/simple-dao/deploy/01-deploy-governor-token.ts
@@ -1,23 +1,43 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/dist/types";
+import { ethers } from "hardhat";
 
 // : DeployFunction (type)
-const deployGovernanceToken: DeployFunction = async function(
-    hre: HardhatRuntimeEnvironment
+const deployGovernanceToken: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
 ) {
-    const {getNamedAccounts, deployments, network} = hre;
-    const {deploy, log} = deployments;
-    const {deployer} = await getNamedAccounts();
-    log("Deploying Governance Token...");
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  log("Deploying Governance Token...");
 
-    const governanceToken = await deploy("GovernanceToken", {
-        from: deployer,
-        args: [],
-        log: true,
-        // waitConfirmations:
-    });
-    //verify
-    log(`Deployed governance token to address ${governanceToken.address}`)
-}
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args: [],
+    log: true,
+    // waitConfirmations:
+  });
+  //verify
+  log(`Deployed governance token to address ${governanceToken.address}`);
+
+  await delegate(governanceToken.address, deployer);
+  log("Delegated..");
+};
+
+// delegate token to deployer
+const delegate = async (
+  governanceTokenAddress: string,
+  delegatedAccount: string
+) => {
+  const governanceToken = await ethers.getContractAt(
+    "GovernanceToken",
+    governanceTokenAddress
+  );
+  const tx = await governanceToken.delegate(delegatedAccount);
+  await tx.wait(1);
+  console.log(
+    `Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`
+  );
+};
 
 export default deployGovernanceToken;
