b0af3403d5cf2d99c4e5caeb6e4aee597ae7de89
Aniket <aniketkumarpaul1@gmail.com>
2023-05-25 22:15:47 +0530

    FastAPI - Break down, Dynamic Paths, Type Conversion

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -3,13 +3,24 @@ from fastapi import FastAPI
 app = FastAPI() 
 
 @app.get('/') # Below function will run for this path
-def index():
+def index(): # multiple function names can be same, but paths must be diff.
     return {
-        'data': {'name': 'Aniket'}
+        'data': 'blog list'
     }
 
-@app.get('/about')
-def about():
+# For /blog/unpublished, it will give error
+# Because, FastAPI reads line by line & since above path comes first & needs int, it will give error for 'unpublished'
+# Thus, this path should come before the int one
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : 'all unpublished blogs'}
+
+# Dynamic routing using {}
+@app.get('/blog/{id}')
+# Default type is string
+# By using :int, we're saying that the id can only be integers, if we give /blog/abc.. it'll give error
+def show(id: int): 
     return {
-        'data': {'about page'}
+        'data': id
     }
+
