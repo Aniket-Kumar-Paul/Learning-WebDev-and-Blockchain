e0398ec6eb2b3e255fbb3d62784d4363aabf283a
Aniket <aniketkumarpaul1@gmail.com>
2023-05-30 10:21:29 +0530

    FastAPI - Store blog to database

--- FastAPI/blog/main.py
+++ FastAPI/blog/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
-
-app = FastAPI() 
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 # Create the tables in database
 models.Base.metadata.create_all(bind=engine)
 
+app = FastAPI() 
+
+def get_db():
+    db = SessionLocal() # create instance of the databse session
+    
+    try:
+        yield db 
+    finally:
+        db.close
+
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request  
+def create(request: schemas.Blog, db: Session = Depends(get_db)): # Default value of db is Depends(get_db) to convert Session to a pydantic type
+    new_blog = models.Blog(
+        title=request.title, 
+        body=request.body
+        )
+    db.add(new_blog)
+    db.commit()
+    # Refresh state of new_blog object with any changes made on db side (like automatic generated IDs etc.)
+    db.refresh(new_blog)
+    return new_blog
--- FastAPI/blog/models.py
+++ FastAPI/blog/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 
 # Blog Model (Table)
 class Blog(Base):
-    __tablename__ = 'myBlogs'
+    __tablename__ = "myBlogs"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
